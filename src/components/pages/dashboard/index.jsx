74b8a3477a3256aa6ca5d87ffaac846f021ef97b
jamiu10012 <ibrahimjamiu026@gmail.com>
2023-07-27 20:56:26 +0100

    navbar destop view done

---
+++ src/components/pages/dashboard/index.jsx
@@ -0,0 +1,11 @@
+import DashNavbar from "../../dashComponents/navbar";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard-container">
+      <DashNavbar />
+    </div>
+  );
+};
+
+export default Dashboard;
