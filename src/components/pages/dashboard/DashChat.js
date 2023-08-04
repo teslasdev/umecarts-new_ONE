ef9c45c1e24604b3bf45b818df313439d4fffa99
jamiu10012 <ibrahimjamiu026@gmail.com>
2023-08-04 21:49:12 +0100

    on message

--- src/components/pages/dashboard/DashChat.js
+++ src/components/pages/dashboard/DashChat.js
@@ -35,66 +35,12 @@ const DashChat = () => {
       </div>
       <div className="chat-main-container">
         <div className="date-chat">Thu 8 Jul 2023</div>
+
         <div className="pro-customer-box">
           <div className="profile-image">
-            <img src={shoe} alt="" />
+            <img src={shoe} alt="" className="cus-pro" />
           </div>
-          <div className="customer-chat-cont">
-            <div className="pro-info">Product Information</div>
-            <div className="img-title-box">
-              <img src={shoe} alt="" />
-              <div className="tit-word">Louis Vuitton Pam slipper</div>
-            </div>
-            <div className="min-msg">
-              Hello! i want to make some few clarifications about this product.
-              How will i get them?
-            </div>
-          </div>
-        </div>
-        <div className="seller-box">
-          <div className="seller-chat-cont ">
-            Good afternoon! you can get them as soon as you order. Delivery
-            takes about 3 days. it will be sent to your location and in the
-            interim, you can use your order/tracking code to track the product
-            untill it gets delivered. Thank you.
-          </div>
-          <div className="profile-image">
-            <img src={shoe} alt="" />
-          </div>
-        </div>
-        <div className="pro-customer-box">
-          <div className="profile-image">
-            <img src={shoe} alt="" />
-          </div>
-          <div className="customer-chat-cont">
-            <div className="min-msg">
-              Hello! i want to make some few clarifications about this product.
-              How will i get them?
-            </div>
-          </div>
-        </div>
-        <div className="seller-box">
-          <div className="seller-chat-cont ">
-            Good afternoon! you can get them as soon as you order. Delivery
-            takes about 3 days. it will be sent to your location and in the
-            interim, you can use your order/tracking code to track the product
-            untill it gets delivered. Thank you.
-          </div>
-          <div className="profile-image">
-            <img src={shoe} alt="" />
-          </div>
-        </div>
-        <div className="pro-customer-box">
-          <div className="profile-image">
-            <img src={shoe} alt="" />
-          </div>
-          <div className="msg-sequential">
-            <div className="customer-chat-cont">
-              <div className="min-msg">
-                Hello! i want to make some few clarifications about this
-                product. How will i get them?
-              </div>
-            </div>
+          <div className="time-msg-sent">
             <div className="customer-chat-cont">
               <div className="pro-info">Product Information</div>
               <div className="img-title-box">
@@ -106,6 +52,75 @@ const DashChat = () => {
                 product. How will i get them?
               </div>
             </div>
+            <div className="time-sent">5:21pm</div>
+          </div>
+        </div>
+
+        <div className="seller-box">
+          <div className="seller-sent-time">
+            <div className="seller-chat-cont ">
+              Good afternoon! you can get them as soon as you order. Delivery
+              takes about 3 days. it will be sent to your location and in the
+              interim, you can use your order/tracking code to track the product
+              untill it gets delivered. Thank you.
+            </div>
+            <div className="time-sent">5:21pm</div>
+          </div>
+
+          <div className="profile-image">
+            <img src={shoe} alt="" />
+          </div>
+        </div>
+        <div className="pro-customer-box">
+          <div className="profile-image">
+            <img src={shoe} alt="" className="cus-pro" />
+          </div>
+          <div className="customer-chat-cont">
+            <div className="min-msg">
+              Hello! i want to make some few clarifications about this product.
+              How will i get them?
+            </div>
+          </div>
+        </div>
+        <div className="seller-box">
+          <div className="seller-sent-time">
+            <div className="seller-chat-cont ">
+              Good afternoon! you can get them as soon as you order. Delivery
+              takes about 3 days. it will be sent to your location and in the
+              interim, you can use your order/tracking code to track the product
+              untill it gets delivered. Thank you.
+            </div>
+            <div className="time-sent">5:21pm</div>
+          </div>
+          <div className="profile-image">
+            <img src={shoe} alt="" />
+          </div>
+        </div>
+        <div className="pro-customer-box">
+          <div className="profile-image">
+            <img src={shoe} alt="" className="cus-pro" />
+          </div>
+          <div className="time-msg-sent">
+            <div className="msg-sequential">
+              <div className="customer-chat-cont">
+                <div className="min-msg">
+                  Hello! i want to make some few clarifications about this
+                  product. How will i get them?
+                </div>
+              </div>
+              <div className="customer-chat-cont">
+                <div className="pro-info">Product Information</div>
+                <div className="img-title-box">
+                  <img src={shoe} alt="" />
+                  <div className="tit-word">Louis Vuitton Pam slipper</div>
+                </div>
+                <div className="min-msg">
+                  Hello! i want to make some few clarifications about this
+                  product. How will i get them?
+                </div>
+              </div>
+            </div>
+            <div className="time-sent">5:21pm</div>
           </div>
         </div>
       </div>
